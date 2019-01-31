ebd66cc23cb652373002901bf8881776575bd970
jonitaajamo <joni.taajamo@outlook.com>
2019-01-31 11:16:41 +0200

    2.14 done

--- osa2/2.12-2.14/maiden-tiedot/src/App.js
+++ osa2/2.12-2.14/maiden-tiedot/src/App.js
@@ -8,21 +8,38 @@ const SearchForm = (props) => (
   </form>
 )
 
-const Country = (props) => (
-  <div>
-    <h1>{props.name}</h1>
-    <p>capital: {props.capital}</p>
-    <p>population: {props.population}</p>
-    <h2>languages</h2>
-    <ul>{props
-        .countries
-        .map(country => country.languages.map(language => <li key={language.name}>{language.name}</li>))}
-    </ul>
-    <p>
-      <img src={props.imageUrl} alt="country flag" height="100" hspace="20"/>
-    </p>
-  </div>
-)
+const Country = (props) => {
+  props.fetchWeather(props.capital)
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p>capital: {props.capital}</p>
+      <p>population: {props.population}</p>
+      <h2>languages</h2>
+      <ul>{props
+          .countries
+          .map(country => country.languages.map(language => <li key={language.name}>{language.name}</li>))}
+      </ul>
+      <p>
+        <img src={props.imageUrl} alt="country flag" height="100" hspace="20"/>
+      </p>
+      <h2>Weather in {props.capital}</h2>
+      <p>
+        <b>temperature:
+        </b>
+        {props.temperature}
+        Celsius
+      </p>
+      <p><img src={props.icon} alt="weather icon"/></p>
+      <p>
+        <b>wind:</b>
+        {props.wind}
+        mps, direction {props.windDirection}
+        degrees
+      </p>
+    </div>
+  )
+}
 
 const CountryList = (props) => {
   const countries = props.filteredCountries
@@ -42,12 +59,18 @@ const App = () => {
 
   const [countryData,
     setCountryData] = useState([])
-
   const [filter,
     setFilter] = useState('')
-
   const [filteredCountries,
     setFilteredCountries] = useState([])
+  const [temperature,
+    setTemperature] = useState('')
+  const [wind,
+    setWind] = useState('')
+  const [icon,
+    setIcon] = useState('')
+  const [windDirection,
+    setWindDirection] = useState('')
 
   useEffect(() => {
     axios
@@ -77,6 +100,18 @@ const App = () => {
     changeFilter(name)
   }
 
+  const fetchWeather = (capital) => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&APPID=143cc3e71f52aefc4fc09113d65a2013`)
+      .then(responseJson => {
+        console.log(responseJson.data.wind.direction)
+        setTemperature(responseJson.data.main.temp)
+        setWind(responseJson.data.wind.speed)
+        setIcon("http://openweathermap.org/img/w/" + responseJson.data.weather[0].icon + ".png")
+        setWindDirection(responseJson.data.wind.direction)
+      })
+  }
+
   return (
     <div>
       <SearchForm filter={filter} filterChangeHandler={filterChangeHandler}/> {filteredCountries.length === 1
@@ -85,7 +120,12 @@ const App = () => {
             name={filteredCountries[0].name}
             capital={filteredCountries[0].capital}
             population={filteredCountries[0].population}
-            imageUrl={filteredCountries[0].flag}/>
+            imageUrl={filteredCountries[0].flag}
+            temperature={temperature}
+            wind={wind}
+            icon={icon}
+            windDirection={windDirection}
+            fetchWeather={fetchWeather}/>
         : <CountryList filteredCountries={filteredCountries} handleClick={handleClick}/>}
     </div>
   )
