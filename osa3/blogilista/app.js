6527ccc0e5af0bf65df1a52f3be3ec861074f089
jonitaajamo <joni.taajamo@outlook.com>
2019-02-18 16:36:52 +0200

    4.8 - 4.14 added

--- osa3/blogilista/app.js
+++ osa3/blogilista/app.js
@@ -4,7 +4,10 @@ const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
+
+console.log('Connecting to', config.MONGODB_URI)
 
 mongoose
     .connect(config.MONGODB_URI, {useNewUrlParser: true})
@@ -14,10 +17,12 @@ mongoose
     .catch((error) => {
         console.log('error connection to MongoDB:', error.message)
     });
-
-app.use(cors());
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- osa3/blogilista/controllers/blogs.js
+++ osa3/blogilista/controllers/blogs.js
@@ -1,17 +1,20 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
+blogsRouter.get('/', (request, response, next) => {
     Blog
         .find({})
         .then(blogs => {
             response.json(blogs)
         })
+        .catch(error => next(error))
+
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', (request, response, next) => {
     const blog = new Blog(request.body)
-
+    if (!blog.likes) 
+        blog.likes = 0
     blog
         .save()
         .then(result => {
@@ -19,6 +22,36 @@ blogsRouter.post('/', (request, response) => {
                 .status(201)
                 .json(result)
         })
+        .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', async(request, response, next) => {
+    try {
+        await Blog.findByIdAndDelete(request.params.id)
+        response
+            .status(204)
+            .end()
+    } catch (error) {
+        next(error)
+    }
+})
+
+blogsRouter.put('/:id', async(request, response, next) => {
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+        response.json(updatedBlog.toJSON())
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = blogsRouter
